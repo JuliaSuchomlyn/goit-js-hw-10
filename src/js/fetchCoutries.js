2e08ed76267881a734b909bccd8d7380cecf9af1
Julia <juliasuchomlyn@gmail.com>
2022-07-25 17:13:06 +0300

    fix fetchCountries

--- src/js/fetchCoutries.js
+++ src/js/fetchCoutries.js
@@ -1,8 +1,11 @@
 function fetchCountries(name) {
     const BASE_URL = 'https://restcountries.com/v3.1/name/';
     return fetch(`${BASE_URL}${name}?fields=name,capital,population,flags,languages`)
-        .then(response => response.json())
-        .catch(error => console.log(error));
-
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        });
 };
 export { fetchCountries };
